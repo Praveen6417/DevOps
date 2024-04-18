68d4093ce2184e88617c4c674aed571d60e180d0
Praveen6417 <praveenkasagalla123@gmail.com>
2024-04-18 12:47:46 +0530

    This is the Installation file

--- for-loop.sh
+++ for-loop.sh
@@ -9,3 +9,12 @@ for fruit in apple banana orange
 do
     echo "Fruit: $fruit"
 done
+
+for i in {1..3}
+do
+    echo "Outer loop iteration: $i"
+    for j in {1..2}
+    do
+        echo "Inner loop iteration: $j"
+    done
+done
