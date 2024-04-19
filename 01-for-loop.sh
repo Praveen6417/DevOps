7263128c0544fe2df23bfd40c1e0bd9854a1103f
Praveen6417 <praveenkasagalla123@gmail.com>
2024-04-19 13:16:38 +0530

    This is the Installation file

--- 01-for-loop.sh
+++ 01-for-loop.sh
@@ -3,7 +3,7 @@ timestamp=$(date +%F-%H-%M-%S)
 script_name=$(echo "$0" | cut -d "." -f1)
 logfile=/tmp/$timestamp-$script_name.log
 
-if[ $USERID -ne 0 ]
+if [ $USERID -ne 0 ]
 then
     echo "Install Packages using root access"
     exit 1
@@ -17,7 +17,7 @@ do
 
     dnf install $i &>> $logfile
 
-    if[ $? -eq 0 ]
+    if [ $? -eq 0 ]
     then
         echo "$i installed successfully"
     else 
