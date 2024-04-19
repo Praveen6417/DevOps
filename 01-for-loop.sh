09080de3c1b3e32098aa22e84e20b851acd8bfa7
Praveen6417 <praveenkasagalla123@gmail.com>
2024-04-19 13:11:26 +0530

    This is the Installation file

---
+++ 01-for-loop.sh
@@ -0,0 +1,26 @@
+USERID=$(id -u)
+timestamp=$(date +%F-%H-%M-%S)
+script_name=$(echo "$0" | cut -d "." -f1)
+logfile=/tmp/$timestamp-$script_name.log
+
+if[ $USERID -nq 0 ]
+then
+    echo "Install Packages using root access"
+    exit 1
+else
+    echo "You are Super User"
+fi
+
+for i in $@
+do 
+    echo "Attempting to install $i"
+
+    dnf install $i &>> $logfile
+
+    if[ $? -eq 0 ]
+    then
+        echo "$i installed successfully"
+    else 
+        echo "Failed to install $i."
+    fi
+done
