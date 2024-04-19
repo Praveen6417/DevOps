85eb8fbf405bf7d2c318bc97a34d9ea2022f99af
Praveen6417 <praveenkasagalla123@gmail.com>
2024-04-19 13:36:57 +0530

    This is the Installation file

---
+++ for-loop-01.sh
@@ -0,0 +1,13 @@
+for i in {1..5}; do
+    if [ $i -eq 3 ]; then
+        continue  # Skip the rest of the loop for i equals 3
+    fi
+    echo "Processing number $i"
+done
+
+for i in {1..5}; do
+    if [ $i -eq 3 ]; then
+        exit 1  # Skip the rest of the loop for i equals 3
+    fi
+    echo "Processing number $i"
+done
