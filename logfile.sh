d9a47682c933b681cc1f21e3a3bd190f6822880e
Praveen6417 <praveenkasagalla123@gmail.com>
2024-04-26 14:43:30 +0530

    This is the Installation file

--- logfile.sh
+++ logfile.sh
@@ -5,10 +5,10 @@ logfiles=/tmp/logs
 if [ -d $logfiles ]
 then    
     echo "Logs directory is already created"
-    exit 1
 else
     mkdir logs
     echo "logs directory created at $logfiles"
+    exit 1
 fi
 
 remove=$(find -type f -name "*.log" -mtime +30)
