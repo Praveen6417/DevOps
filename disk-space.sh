7a61c130d773f4db3889a4573dbf80016d3a59f6
Praveen6417 <praveenkasagalla123@gmail.com>
2024-04-27 15:23:41 +0530

    This is the Installation file

--- disk-space.sh
+++ disk-space.sh
@@ -1,11 +1,17 @@
 Disk_usage=$(df -hT | grep xfs)
+Disk_max=75
 
 while IFS= read -r line
 do 
     usage=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    folder=$(echo $line | awk -F " " '{print $NF}')
+    if [ usage -gt Disk_max ]
+    then
+        echo "$folder usage is more than $Disk_max, Current usage : $usage"
+    else
+        echo "Current usage :$usage"
+    fi
 
 done <<< $Disk_usage
-
-echo "$usage"
 
 echo "Hii"
