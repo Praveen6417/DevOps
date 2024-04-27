ee959927b5f5bf012feda460e435359e1ae23fa9
Praveen6417 <praveenkasagalla123@gmail.com>
2024-04-27 16:10:57 +0530

    This is the Installation file

--- disk-space.sh
+++ disk-space.sh
@@ -3,7 +3,7 @@ Disk_max=75
 
 while IFS= read -r line
 do 
-    usage=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    usage+=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     # folder=$(echo $line | awk -F " " '{print $NF}')
     # if [ $usage -gt $Disk_max ]
     # then
