141068ffe8221b5693a185caf565867f5eb2474d
Praveen6417 <praveenkasagalla123@gmail.com>
2024-04-27 14:16:37 +0530

    This is the Installation file

--- disk-space.sh
+++ disk-space.sh
@@ -2,6 +2,8 @@ Disk_usage=$(df -hT | grep xfs)
 
 while IFS=read -r line
 do 
-    echo "$line"
-    
+    usage=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+
 done < $Disk_usage
+
+echo $usage
